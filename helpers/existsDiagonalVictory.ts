f149a49ebece926402243d1b39a4ab622845bd9f
GTI/WILLYAM.PIMENTEL <10482656417@sdspe.infra>
2022-10-24 17:19:47 -0300

    feat: terminando funções com condição de vitória

--- helpers/existsDiagonalVictory.ts
+++ helpers/existsDiagonalVictory.ts
@@ -1,5 +1,5 @@
 export const existsDiagonalVictory = (matrix: string[][], player: string) =>{
-    if ((matrix[0][0] == matrix[1][1] && matrix[0][0] == matrix[2][2]) || (matrix[0][2] == matrix[1][1] && matrix[0][2] == matrix[2][0])){
+    if ((matrix[0][0] == matrix[1][1] && matrix[0][0] == matrix[2][2] && matrix[0][0] != '_') || (matrix[0][2] == matrix[1][1] && matrix[0][2] == matrix[2][0] && matrix[0][2] != '_')){
       return true
     }
     else{
