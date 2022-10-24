62f491c2582c2c6ab374ce5819fa069ad25fb8c7
GTI/WILLYAM.PIMENTEL <10482656417@sdspe.infra>
2022-10-24 16:22:42 -0300

    feat: movendo helpers pro src

---
+++ src/helpers/existsDiagonalVictory.ts
@@ -0,0 +1,8 @@
+export const existsDiagonalVictory = (matrix: string[][], player: string) =>{
+    if ((matrix[0][0] == matrix[1][1] && matrix[0][0] == matrix[2][2]) || (matrix[0][2] == matrix[1][1] && matrix[0][2] == matrix[2][0])){
+      return true
+    }
+    else{
+      return false
+    }
+  } 
